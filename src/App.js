53e17e59c29d767a76d52e84ba9348fe34a9df76
Eleni Danessis <edanessis@Elenis-MacBook-Air.local>
2022-10-21 22:36:53 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import Pages from "./pages/Pages";
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Pages/>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
