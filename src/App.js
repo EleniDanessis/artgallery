3b4d2bf3721e7433eaf3791db6b93824c860f80b
Eleni Danessis <edanessis@Elenis-MacBook-Air.local>
2022-10-22 17:54:39 -0400

    debugging

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 
 import Pages from "./pages/Pages";
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Helmet} from "react-helmet"
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Helmet>
-          <title>Art Gallery</title>
-          <meta 
-            name="description"
-            content="Eleni Danessis's Art Gallery React Project" 
-          />
-        </Helmet>
+      <HashRouter>
         <Pages/>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
